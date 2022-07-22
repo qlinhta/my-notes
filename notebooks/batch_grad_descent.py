1ac490f6dfff270aae74d1aa319e2a3719b6e4e3
Quyen Linh TA <qlinhta@outlook.com>
2022-07-22 16:23:00 +0200

    Update :: Compare regression @ running pc.c

--- notebooks/batch_grad_descent.py
+++ notebooks/batch_grad_descent.py
@@ -1,5 +1,5 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 rate = 0.05  # learning rate
 
--- notebooks/mini_batch_grad_descent.py
+++ notebooks/mini_batch_grad_descent.py
@@ -1,6 +1,7 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import random
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 rate = 0.05  # learning rate
 
