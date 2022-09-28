a9f5c91b65a96c3ef21dc85fd20c9564fd9f1021
Quyen Linh TA <qlinhta@outlook.com>
2022-09-28 23:27:56 +0200

    Update :: Addition Shortest Path @ Completed

---
+++ algorithms/bubble-short.py
@@ -0,0 +1,12 @@
+# Bubble short algorithm
+
+def bubble_short(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+if __name__ == "__main__":
+    arr = [1, 5, 2, 4, 3]
+    print(bubble_short(arr))
