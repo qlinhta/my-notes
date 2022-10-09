f6cbebf71029a3ed95926bdc572bc08216d8a9e7
Quyen Linh TA <qlinhta@outlook.com>
2022-10-09 12:29:41 +0200

    Update :: Addition @ CNN MNIST

---
+++ Area51/CNN.py
@@ -0,0 +1,130 @@
+# Build the convolutional neural network model for the MNIST dataset
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+import time
+import os
+import sys
+
+
+# Define the convolutional neural network
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+# Define the training function
+def train(net, trainloader, criterion, optimizer, epoch):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # Get the inputs
+        inputs, labels = data
+
+        # Zero the parameter gradients
+        optimizer.zero_grad()
+
+        # Forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # Print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:  # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+
+# Define the testing function
+def test(net, testloader):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10000 test images: %d %%' % (
+            100 * correct / total))
+
+
+# Define the main function
+def main():
+    # Define the device
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("Device: ", device)
+
+    # Define the hyperparameters
+    batch_size = 4
+    learning_rate = 0.001
+    num_epochs = 2
+
+    # Define the transform
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5,), (0.5,))])
+
+    # Define the training set and the training loader
+    trainset = torchvision.datasets.MNIST(root='./data', train=True,
+                                          download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                              shuffle=True, num_workers=2)
+
+    # Define the testing set and the testing loader
+    testset = torchvision.datasets.MNIST(root='./data', train=False,
+                                         download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                             shuffle=False, num_workers=2)
+
+    # Define the classes
+    classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+    # Define the network
+    net = CNN()
+    net.to(device)
+
+    # Define the loss function and the optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9)
+
+    # Train the network
+    for epoch in range(num_epochs):
+        train(net, trainloader, criterion, optimizer, epoch)
+
+    # Test the network
+    test(net, testloader)
+
+    # Save the model
+    torch.save(net.state_dict(), "model.pth")
+    print("Model saved!")
+
+
+# Define the entry point
+if __name__ == "__main__":
+    main()
