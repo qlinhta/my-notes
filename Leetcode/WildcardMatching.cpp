88115bae04120bf96a3a3b1453ae96654e64d01e
Quyen Linh TA <qlinhta@outlook.com>
2023-02-02 22:08:54 +0100

    Update :: Addition @ Remove caches

---
+++ Leetcode/WildcardMatching.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Quyen Linh TA on 02/02/2023.
+//
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size(), n = p.size();
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        dp[0][0] = true;
+        for (int i = 1; i <= n; i++) {
+            if (p[i - 1] == '*') {
+                dp[0][i] = dp[0][i - 1];
+            }
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else if (p[j - 1] == '?' || s[i - 1] == p[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
